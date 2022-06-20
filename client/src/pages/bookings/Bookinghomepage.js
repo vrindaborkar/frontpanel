f93986770b7a4c8f46b516b6799c9acc72a6f764
unknown <admin@IN-50040.tox-de.com>
2022-06-20 13:02:46 +0530

    today's changes

---
+++ client/src/pages/bookings/Bookinghomepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import './stallbookings.css'
+import './Bookings.css'
+
+const Bookinghomepage = () => {
+  return (
+    <div className='bookings_main'>
+        <h2>Stall Booking Platform</h2>
+        <br/>
+        <Link to={'./vendorsfeed'}className="vendor_form">Vendor's Requirement</Link>
+        <br/>
+        <Link to={'./bookingstall'}className="book_stall">Book Stall</Link>
+    </div>
+  )
+}
+
+export default Bookinghomepage
+
+// style={{marginTop:"13vh" , fontSize:"1.5rem"}}
